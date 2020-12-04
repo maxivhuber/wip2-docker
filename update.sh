d1a050f2b09c84ae9b43058c6b9981ed303287ac
Maximilian Huber <52167836+maxivhuber@users.noreply.github.com>
2020-12-04 12:01:48 +0100

    Update update.sh
    final

--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+#exit if a command fails
+set -e
+
 PATCH=$1
 REPOSITORY=$2
 BRANCH=$3
 clear
 
+#check for variables
 if [[ -z $PATCH ]] || [[ -z $REPOSITORY ]];
 then
     printf "do not call this manually\n"
@@ -18,7 +23,7 @@ patch -p0 < ${PATCH}
 rm $HOME/.patch/${PATCH}
 cd $HOME/.patch/${REPOSITORY}
 git add .
-git commit -m ${PATCH}
+git commit -m ${PATCH} -q
 
 read -p "Push changes automatically? [y/n]" -n 1 -r
 printf "\n"
