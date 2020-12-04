f592e390d582c21a3f53be39800ef54ef63c8353
Maximilian Huber <52167836+maxivhuber@users.noreply.github.com>
2020-12-04 11:42:59 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,19 +2,27 @@
 PATCH=$1
 REPOSITORY=$2
 clear
+
+if [[ -z $PATCH ]] || [[ -z $REPOSITORY ]];
+then
+    printf "do not call this manually\n"
+    exit
+fi
+
 cp $HOME/.patch/patches/${PATCH} $HOME/.patch/${PATCH}
 cd $HOME/.patch
 patch -p0 < ${PATCH}
 rm $HOME/.patch/${PATCH}
 cd $HOME/.patch/${REPOSITORY}
 git add .
+git commit -m ${PATCH}
 
-read -p "Commit changes automatically? [y/n]" -n 1 -r
+read -p "Push changes automatically? [y/n]" -n 1 -r
 printf "\n"
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    git commit -m ${PATCH}
+    git push
 else
-    printf "pls commit your changes manually\n"
+    printf "pls push your changes manually\n"
     exit
 fi
 exit
